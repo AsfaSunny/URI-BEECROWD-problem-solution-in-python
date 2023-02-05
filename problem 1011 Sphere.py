8ef6b02c6997a2b156dad6c28d55b4a7b00d4bb7
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-02-06 00:02:29 +0600

    Update problem 1011 Sphere.py

--- problem 1011 Sphere.py
+++ problem 1011 Sphere.py
@@ -4,3 +4,13 @@ pi = 3.14159
 volume = float(4.0 * pi * (ratio* ratio * ratio) / 3)
 
 print(f'VOLUME = {volume:.3f}')
+
+
+
+
+
+# alternative way:
+
+R = int(input())
+SPHERE = (4/3.0*3.14159*R*R*R)
+print("VOLUME = %0.3f"%SPHERE)
