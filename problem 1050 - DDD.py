4c397a073cb715206ee6271c897662902b2ffdfd
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-03-09 22:09:09 +0600

    BEECROWD 1050

---
+++ problem 1050 - DDD.py
@@ -0,0 +1,20 @@
+A = int(input())
+
+if A==61:
+    print("Brasilia")
+elif A==71:
+    print("Salvador")
+elif A==11:
+    print("Sao Paulo")
+elif A==21:
+    print("Rio de Janeiro")
+elif A==32:
+    print("Juiz de Fora")
+elif A==19:
+    print("Campinas")
+elif A==27:
+    print("Vitoria")
+elif A==31:
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
